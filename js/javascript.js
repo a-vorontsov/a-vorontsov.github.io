fedf0ef6dbee434b2486ff9d52d97ce91e537deb
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2018-09-03 12:24:47 +0100

    Update everything

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,33 @@
-$(document).on('click', '.navLink', function(e) {
-    e.preventDefault();
-    var target = $(this).attr('href');
-    var targetOffset = $(target).offset();
-    $('html,body').animate({
-        scrollTop: (targetOffset.top)
-    }, 500)
-});
+const normalScrollElements = (window.innerWidth < 1150) ? '.scrollable-element' : null;
+const paddingTop = (window.innerWidth < 992) ? '1em' : '3em';
 
-$(document).on('click', '.scrollTop', function(e) {
-    e.preventDefault();
-    $('html,body').animate({
-        scrollTop: (0)
-    }, 500
-    );
-});
-
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 200) {
-        $('.upArrow').fadeIn();
-    } else {
-        $('.upArrow').fadeOut();
-    }
+new fullpage('#fullpage', {
+  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  scrollHorizontally: true,
+  css3: true,
+  scrollingSpeed: 700,
+  fitToSection: true,
+  fitToSectionDelay: 1000,
+  scrollBar: false,
+  easing: 'easeInOut',
+  easingcss3: 'ease',
+  loopHorizontal: false,
+  interlockedSlides: false,
+  dragAndMove: true,
+  resetSliders: false,
+  touchSensitivity: 15,
+  normalScrollElementTouchThreshold: 3,
+  normalScrollElements,
+  bigSectionsDestination: null,
+  keyboardScrolling: true,
+  animateAnchor: true,
+  recordHistory: true,
+  controlArrows: true,
+  verticalCentered: false,
+  paddingTop,
+  paddingBottom: '10px',
+  responsiveSlides: true,
+  sectionSelector: '.section',
+  slideSelector: '.slide',
+  sectionsColor: ['#CE9FFC', '#fff', '#efefef', '#fff', '#efefef', '#fff',]
 });
