4adf00461db416d6dfee08984e0ab00b69d47173
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2020-08-03 00:27:00 +0100

    Updates

---
+++ precache-manifest.c07a0fccd12905914df0f28634aa1256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fb05b512ebedd9f34246d5cbf64b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7952937b707fcd26a46c",
+    "url": "/static/js/2.6bacc19a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bacc19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7944beeda5423c1e210a",
+    "url": "/static/js/main.bfc549ce.chunk.js"
+  },
+  {
+    "revision": "15e73e21f083f18850f4",
+    "url": "/static/js/runtime-main.9fd15c81.js"
+  }
+]);
