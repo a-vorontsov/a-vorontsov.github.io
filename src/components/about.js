2f4a97b95cf23c9150d5f99d028384735e6f4a5f
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2021-06-08 14:27:17 +0100

    Update MSci to BSc

--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ export default function About() {
         <div className="horizontal-center col-12 col-md-10 col-md-offset-1">
           <h2>About</h2>
           <p>
-            I am a developer and MSci Computer Science student at King's College
+            I am a developer and BSc (Hons) Computer Science student at King's College
             London.
           </p>
         </div>
