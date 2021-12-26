8369af40676f0652c837fed1a589d4f5d4b0ffbb
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2021-12-26 19:36:26 +0000

    Update styling and font requests

--- projects.js
+++ projects.js
@@ -1,23 +1,23 @@
 exports.array = [
   {
-    "name": "Aver",
-    "url": "https://github.com/a-vorontsov/aver",
-    "image": "/images/aver.webp",
-    "desc": "Programming language developed as part of Bachelor's degree. Compiler developed in <a href='https://ocaml.org/' target='_blank' rel='noopener noreferrer'>OCaml</a>, VM developed in <a href='https://rpython.readthedocs.io/en/latest/' target='_blank' rel='noopener noreferrer'>RPython</a>"
+    name: "Aver",
+    url: "https://github.com/a-vorontsov/aver",
+    image: "/images/aver.webp",
+    desc: "Programming language developed as part of Bachelor's degree. Compiler developed in <a class='bold' href='https://ocaml.org/' target='_blank' rel='noopener noreferrer'>OCaml</a>, VM developed in <a class='bold' href='https://rpython.readthedocs.io/en/latest/' target='_blank' rel='noopener noreferrer'>RPython</a>",
   },
   {
-    "name": "weather",
-    "url": "https://github.com/a-vorontsov/weather",
-    "weburl": "https://weather.vorontsov.co.uk/",
-    "image": "/images/weather.webp",
-    "desc": "CLI tool for getting the weather forecast at your current location or a specified location. Developed in <a href='https://nim-lang.org/' target='_blank' rel='noopener noreferrer'>Nim</a>, uses <a href='https://openweathermap.org/' target='_blank' rel='noopener noreferrer'>OpenWeatherMap API</a> and <a href='https://freegeoip.app/' target='_blank' rel='noopener noreferrer'>freegeoip</a>"
+    name: "weather",
+    url: "https://github.com/a-vorontsov/weather",
+    weburl: "https://weather.vorontsov.co.uk/",
+    image: "/images/weather.webp",
+    desc: "CLI tool for getting the weather forecast at your current location or a specified location. Developed in <a class='bold' href='https://nim-lang.org/' target='_blank' rel='noopener noreferrer'>Nim</a>, uses <a class='bold' href='https://openweathermap.org/' target='_blank' rel='noopener noreferrer'>OpenWeatherMap API</a> and <a class='bold' href='https://freegeoip.app/' target='_blank' rel='noopener noreferrer'>freegeoip</a>",
   },
   {
-    "name": "git-gud",
-    "url": "https://github.com/a-vorontsov/git-gud",
-    "weburl": "https://git-gud.vorontsov.co.uk/",
-    "image": "/images/git-gud.webp",
-    "desc": "CLI tool developed to aid in usage of git commands. git-gud processes a git command, takes the error and gets an answer using the <a href='https://api.stackexchange.com/docs' target='_blank' rel='noopener noreferrer'>stackoverflow API</a>. Written in Go, original implementation in Python."
+    name: "git-gud",
+    url: "https://github.com/a-vorontsov/git-gud",
+    weburl: "https://git-gud.vorontsov.co.uk/",
+    image: "/images/git-gud.webp",
+    desc: "CLI tool developed to aid in usage of git commands. git-gud processes a git command, takes the error and gets an answer using the <a class='bold' href='https://api.stackexchange.com/docs' target='_blank' rel='noopener noreferrer'>stackoverflow API</a>. Written in Go, original implementation in Python",
   },
   // {
   //   "name": "SashaMaps",
@@ -38,11 +38,11 @@ exports.array = [
   //   "desc": "Web App/Game"
   // },
   {
-    "name": "FilmSpottr",
-    "url": "https://github.com/a-vorontsov/FilmSpottr",
-    "weburl": "https://filmspottr.vorontsov.co.uk",
-    "image": "/images/filmspottr.webp",
-    "desc": "Maps film scenes to real life locations using <a href='https://developers.google.com/maps' target='_blank' rel='noopener noreferrer'>Google maps</a> and imdb data. Scrapes imdb listings for location metadata and maps to coordinates"
+    name: "FilmSpottr",
+    url: "https://github.com/a-vorontsov/FilmSpottr",
+    weburl: "https://filmspottr.vorontsov.co.uk",
+    image: "/images/filmspottr.webp",
+    desc: "Maps film scenes to real life locations using <a class='bold' href='https://developers.google.com/maps' target='_blank' rel='noopener noreferrer'>Google maps</a> and imdb data. Scrapes imdb listings for location metadata and maps to coordinates",
   },
   // {
   //   "name": "Shirt-or-not-Shirt",
@@ -52,17 +52,17 @@ exports.array = [
   //   "desc": "Web App"
   // },
   {
-    "name": "Particle",
-    "url": "https://github.com/a-vorontsov/Particle",
-    "image": "/images/particle.webp",
-    "desc": "Desktop game made for BAFTA Young Game Designers 2017. Written in Unity using C#. Custom physics and movement developed as part of game features"
+    name: "Particle",
+    url: "https://github.com/a-vorontsov/Particle",
+    image: "/images/particle.webp",
+    desc: "Desktop game made for BAFTA Young Game Designers 2017. Written in Unity using C#. Custom physics and movement developed as part of game features",
   },
   {
-    "name": "RockPaperSplat-web",
-    "url": "https://github.com/a-vorontsov/rockpapersplat-web",
-    "weburl": "https://rockpapersplat.vorontsov.co.uk",
-    "image": "/images/rps.webp",
-    "desc": "Web page for RockPaperSplat written in plain HTML+CSS+JS. Includes simple pixel art editor to save artwork to device"
+    name: "RockPaperSplat-web",
+    url: "https://github.com/a-vorontsov/rockpapersplat-web",
+    weburl: "https://rockpapersplat.vorontsov.co.uk",
+    image: "/images/rps.webp",
+    desc: "Web page for RockPaperSplat written in plain HTML+CSS+JS. Includes simple pixel art editor to save artwork to device",
   },
   // {
   //   "name": "GoLink",
@@ -70,4 +70,4 @@ exports.array = [
   //   "image": "/images/golink.webp",
   //   "desc": "Mobile App"
   // }
-]
+];
