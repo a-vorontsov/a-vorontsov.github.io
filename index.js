baef021401ce01b4b979338a40026b282c0afb2b
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2025-03-13 12:20:37 +0000

    update styling

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const CleanCSS = require("clean-css");
+
 const fs = require("fs"),
   ejs = require("ejs"),
   paths = require("path"),
   minify = require("html-minifier").minify,
+  cleanCss = require("clean-css"),
   projs = require("./projects");
 
 function ejsToHTML(path) {
@@ -35,4 +38,20 @@ function ejsToHTML(path) {
   );
 }
 
+function minifyCss(path) {
+  const output = new CleanCSS({level: 2}).minify([path]);
+  fs.writeFile(
+    paths.resolve(__dirname, "public", "css", "stylesheet.min.css"),
+    output.styles,
+    function (err) {
+      if (err) {
+        console.log(err);
+        return false;
+      }
+      return true;
+    }
+  );
+}
+
 ejsToHTML(__dirname + "/src/index.ejs");
+minifyCss(__dirname + "/public/css/stylesheet.css");
